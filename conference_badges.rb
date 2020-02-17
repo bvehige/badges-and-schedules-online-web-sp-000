b8cf89debefd49eba6863b2da65963ecfabd8bf0
Brian Vehige <brianvehige@gmail.com>
2020-02-17 00:24:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,8 +7,7 @@ end
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   
 def batch_badge_creator(attendees)
-  attendees.collect << badge_messages 
-  badge_messages.each do |name|
+  attendees.collect do 
     badge_maker(name)
   end
 end
