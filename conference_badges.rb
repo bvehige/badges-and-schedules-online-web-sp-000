41f2efbaff19db237445a57228fc6eb1f6c03443
Brian Vehige <brianvehige@gmail.com>
2020-02-17 01:01:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map {|attendees, index| "Hello, #{attendees}!  You'll be assigned to room #{index + 1}!"}
+  attendees.each_with_index.map {|attendees, index| "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"}
 end
     
