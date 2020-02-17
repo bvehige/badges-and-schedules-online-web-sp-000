eac52a708f5502c5d24261c851cd8dd531101632
Brian Vehige <brianvehige@gmail.com>
2020-02-17 00:20:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,10 @@
 # Write your code here.
 
-def badge_maker2(speaker)
- return "Hello, my name is #{speaker}."
-end
-
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+  
   
 def batch_badge_creator(names)
   badge_messages = []
